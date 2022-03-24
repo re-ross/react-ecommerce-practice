37e51d1a6f81ff9c3d746db29baac0b7096b01d0
Ryan Ross <ryan@reross.com>
2022-03-24 11:26:47 -0500

    install commerce.js, removed hardcoded products, using API in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-// import Products from "./components/products/Products";
-// import Navbar from "./components/Navbar/Navbar";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
 import { Products, NavBar } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(products);
   return (
     <div>
-      <Products />
       <NavBar />
+      <Products products={products} />
     </div>
   );
 };
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -3,27 +3,10 @@ import { Grid } from "@material-ui/core";
 import Product from "./product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "shoes",
-    description: "running Shoes",
-    price: "$1",
-    image:
-      "https://www.rei.com/dam/content_team_010818_52427_htc_running_shoes_hero2_lg.jpg",
-  },
-  {
-    id: 2,
-    name: "macbook",
-    description: "computer",
-    price: "$10",
-    image:
-      "https://www.mobigyaan.com/wp-content/uploads/2020/03/Apple-Macbook-Air-2020-1.jpg",
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
+
+  console.log(products);
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
