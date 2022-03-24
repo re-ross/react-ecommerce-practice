e226c789c6f01a5c6f7a69086e68d64dff5ea203
Ryan Ross <ryan@reross.com>
2022-03-24 09:53:35 -0500

    initialized project and created structure

---
+++ src/components/products/product/Product.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+const Product = () => {
+  return <div>Product</div>;
+};
+
+export default Product;
